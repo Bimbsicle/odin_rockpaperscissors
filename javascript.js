629fedd4a3412b2d5fcf747b9ca38fd80e4009cb
bimbsicle <118282281+Bimbsicle@users.noreply.github.com>
2022-11-22 21:55:12 +0800

    add RockPaperScissors function

--- javascript.js
+++ javascript.js
@@ -3,4 +3,36 @@ function getComputerChoice() {
     return pcChoice[Math.floor(Math.random() * 2)];
 }
 
-console.log(getComputerChoice());
+function RockPaperScissors(computerSelection, playerSelection){
+    let playerChoice = playerSelection.toUpperCase();
+
+    if (playerChoice === "ROCK" && computerSelection === "Rock") {
+        let result = "Tie! You both chose Rock"
+        return result;
+    } else if (playerChoice === "ROCK" && computerSelection === "Paper") {
+        let result = "You Lose! Paper beats Rock"
+        return result;
+    } else if (playerChoice === "ROCK" && computerSelection === "Scissors") {
+        let result = "You Win! Rock beats Scissors"
+        return result;
+    } else if (playerChoice === "PAPER" && computerSelection === "Paper") {
+        let result = "Tie! You both chose Paper"    
+        return result;
+    } else if (playerChoice === "PAPER" && computerSelection === "Scissors") {
+        let result = "You Lose! Scissor beats Paper"
+        return result;
+    } else if (playerChoice === "PAPER" && computerSelection === "Rock"){
+        let result = "You Win! Paper beats Rock"
+        return result;
+    } else if (playerChoice === "SCISSORS" && computerSelection === "Scissors"){
+        let result = "Tie! You both chose Scissors"
+        return result;
+    } else if (playerChoice === "SCISSORS" && computerSelection === "Rock"){
+        let result = "You Lose! Rock beats Scissors"
+        return result;
+    } else {
+        let result = "You Win! Scissors beats Paper"
+        return result;
+    }
+
+}
