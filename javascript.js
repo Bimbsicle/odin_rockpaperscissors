75ecac14b5a76d62bd4e117db9ae9e8737b9115c
Miggy <118282281+Bimbsicle@users.noreply.github.com>
2022-12-02 01:03:44 +0800

    Add announcer and style edits

--- javascript.js
+++ javascript.js
@@ -4,42 +4,73 @@ const scissorsBtn = document.querySelector('#scissorsbtn')
 
 const scoreboard = document.querySelector('.scoreboard')
 const announcer = document.querySelector('.announcer')
+const instructions = document.querySelector('.instructions')
 
 let playerPoint = 0
 let pcPoint = 0
 
-rockbtn.addEventListener('click', game)
-paperbtn.addEventListener('click', game)
-scissorsbtn.addEventListener('click', game)
+instructions.innerHTML = 'Choose a move against the PC'
 
-function game(){
-    announcer.innerHTML = ''
-    let result = playRound(getComputerChoice(), 'Rock')
-    if (result === 'true'){ 
-        playerPoint += 1
-        announcer.innerHTML = 'You Won!'
-    }
-    else if (result === 'false'){
-        pcPoint += 1
-        announcer.innerHTML = 'You Lost!'
-    }
-    else {
-        playerPoint += 0
-        announcer.innerHTML = 'You Tied!'
-    }
-    
-    scoreCheck(playerPoint, pcPoint)
+rockbtn.addEventListener('click',() => {
+    game('Rock')
+})
+paperbtn.addEventListener('click', () => {
+    game('Paper')
+})
+scissorsbtn.addEventListener('click', () => {
+    game('Scissors')
+})
+
+function game(playerAnswer){
+    instructions.innerHTML = 'Choose your next Move'
+    let point = ''
+    let outcome = playRound(getComputerChoice(), playerAnswer)
+    winnerAnnounce(outcome)
     scoreboard.innerHTML = `Your Score: ${playerPoint} VS PC Score: ${pcPoint}`
+}
+
+function winnerAnnounce(result) {
+    if (result === 'true1') {
+        announcer.innerHTML = 'Rock beats Scissors you won!'
+        playerPoint += 1
+    } else if (result === 'false1') {
+        announcer.innerHTML = 'Paper beats Rock you lost!'
+        pcPoint += 1
+    } else if (result === 'null1') {
+        announcer.innerHTML = 'You both chose Rock it\'s a tie!'
+        playerPoint += 0
+    } else if (result === 'true2') {
+        announcer.innerHTML = 'Paper beats Rock you won!'
+        playerPoint += 1
+    } else if (result === 'false2') {
+        announcer.innerHTML = 'Scissors beats Paper you lost!'
+        pcPoint += 1
+    } else if (result === 'null2') {
+        announcer.innerHTML = 'You both chose Paper it\'s a tie!'
+        playerPoint += 0
+    } else if (result === 'true3') {
+        announcer.innerHTML = 'Scissors beats Paper you won!'
+        playerPoint += 1
+    } else if (result === 'false3') {
+        announcer.innerHTML = 'Rock beats Scissors you lost!'
+        pcPoint += 1
+    } else {
+        announcer.innerHTML = 'You both chose Scissors it\'s a tie!'
+        playerPoint += 0
+    }
+    scoreCheck(playerPoint, pcPoint)
 }
 
 function scoreCheck(playerScore, pcScore){
     if (playerScore >= 5) {
         announcer.innerHTML = `Congratulations, you reached 5 points!`
+        instructions.innerHTML = 'Choose an answer against the PC'
         playerPoint = 0
         pcPoint = 0
     }
     else if (pcScore >=5) {
         announcer.innerHTML = `Unlucky, the computer reached 5 points!`
+        instructions.innerHTML = 'Choose an answer against the PC'
         playerPoint = 0
         pcPoint = 0
     }
@@ -54,22 +85,22 @@ function playRound(computerSelection, playerSelection){
     let playerChoice = playerSelection.toUpperCase();
 
     if (playerChoice === 'ROCK' && computerSelection === 'Rock') {
-        return null
+        return 'null1'
     } else if (playerChoice === 'ROCK' && computerSelection === 'Paper') {
-        return 'false'
+        return 'false1'
     } else if (playerChoice === 'ROCK' && computerSelection === 'Scissors') {
-        return 'true'
+        return 'true1'
     } else if (playerChoice === 'PAPER' && computerSelection === 'Paper') {
-        return null
+        return 'null2'
     } else if (playerChoice === 'PAPER' && computerSelection === 'Scissors') {
-        return 'false'
+        return 'false2'
     } else if (playerChoice === 'PAPER' && computerSelection === 'Rock'){
-        return 'true'
+        return 'true2'
     } else if (playerChoice === 'SCISSORS' && computerSelection === 'Scissors'){
-        return null
+        return 'null3'
     } else if (playerChoice === 'SCISSORS' && computerSelection === 'Rock'){
-        return 'false'
+        return 'false3'
     } else {
-        return 'true'
+        return 'true3'
     }
 }
